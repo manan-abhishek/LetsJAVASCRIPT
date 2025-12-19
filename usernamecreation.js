bd5929fa49583bf830261ce0ef3f884bbfde0de3
Abhishek Kumar Nayak <kumarabhi829121@gmail.com>
2025-12-20 04:30:06 +0530

    Username creation code

---
+++ usernamecreation.js
@@ -0,0 +1,7 @@
+//Prompt the user to enter their fullname.Generate a username  for them based on the input . Start username with @ followed by their fullname and ending with the fullname length.
+
+let fullName = prompt("Enter your FullName")
+console.log(fullName)
+
+let  username = "@"+fullName+fullName.length
+console.log(username)
