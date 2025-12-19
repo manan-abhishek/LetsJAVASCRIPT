50395a7e381f794606657371f686bcb487d8e09c
Abhishek Kumar Nayak <kumarabhi829121@gmail.com>
2025-12-20 04:28:08 +0530

    Initial commit

---
+++ first.js
@@ -0,0 +1,21 @@
+// //Number guessing game
+
+// let gameNumber = 25
+//  let userNum=prompt("Guess the number")
+
+// while(gameNumber != userNum){
+//     userNum = prompt("You guessed it wrong!, Try again")
+    
+//     }
+//     console.log("Congrats you guessed it right!")
+
+
+
+//Prompt the user to enter their fullname.Generate a username  for them based on the input . Start username with @ followed by their fullname and ending with the fullname length.
+
+let fullName = prompt("Enter your FullName")
+console.log(fullName)
+
+let  username = "@"+fullName+fullName.length
+console.log(username)
+
